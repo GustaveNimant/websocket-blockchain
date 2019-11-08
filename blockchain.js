f79df7286cd9ac271f00be433ed3154532952345
GustaveNimant <achadde@iabloko.fecamp>
2019-11-08 10:47:17 +0100

    Le Bloc Genesis ets le plus ancien

--- blockchain.js
+++ blockchain.js
@@ -7,9 +7,16 @@ var O = require("./outils");
 
 var ModuleName = 'blockchain.js';
 
+var MessageType = {
+    QUERY_LATEST: 0,
+    QUERY_ALL: 1,
+    RESPONSE_BLOCKCHAIN: 2
+};
+
 var addBlock = (newBlock, caller) => {
     var here = O.functionNameJS (ModuleName);
-    console.log('\nEntrée dans',here,'appelé par',caller,'avec newBlock',newBlock);
+    console.log('Entrée dans',here,'appelé par',caller)
+    console.log('Entrée dans',here,'avec newBlock',newBlock)
 
     if (isValidNewBlock(newBlock, getLatestBlock(), here)) {
         A.blockChain.push(newBlock);
@@ -17,13 +24,27 @@ var addBlock = (newBlock, caller) => {
     console.log('Sortie  de',here);
 };
 
+var broadcast = (message, caller) => {
+    var here = O.functionNameJS(ModuleName);
+    console.log('\n');
+    console.log('Entrée dans',here,'appelé par',caller,'avec message',message)
+    console.log('dans',here,'il y a',A.socket_a.length,'sockets');
+
+    A.socket_a.forEach (soc =>
+			write(soc, message, here)
+		       );
+    console.log('Sortie  de',here);
+}
+
 var getGenesisBlock = () => {
     var nextHash = O.calculateHash(0, "hash vide", nextTimestamp, "mon bloc génésis");
     var nextTimestamp = new Date().getTime() / 1000;
-
+    var http_port = process.env.HTTP_PORT;
+    var p2p_port = process.env.P2P_PORT;
+    var contenu = "Bloc Genesis de websocket-blockchain http " + http_port + " p2p " + p2p_port;
     return new Block(0,
 		     "texte",
-		     "bloc genesis de websocket-blockchain",
+		     contenu,
 		     nextTimestamp,
 		     "clé publique",
 		     "hash vide",
@@ -34,13 +55,33 @@ var getLatestBlock = () => O.getLatestElement(A.blockChain);
 
 var isValidChain = (blockchainToValidate) => {
     var here = O.functionNameJS(ModuleName);
+    console.log('\n');
     console.log('Entrée dans',here,'avec blockchainToValidate',blockchainToValidate);
+    var genesisBlockToValidate = blockchainToValidate[0];
+    var genesisBlockCurrent = getGenesisBlock();
+    
+    if (JSON.stringify(genesisBlockToValidate) !== JSON.stringify(genesisBlockCurrent)) {
+	console.log('dans',here,'genesisBlockToValidate',genesisBlockToValidate);
+	console.log('dans',here,'tandis que genesisBlockCurrent',genesisBlockCurrent);
 
-    if (JSON.stringify(blockchainToValidate[0]) !== JSON.stringify(getGenesisBlock())) {
-	console.log('\nErreur dans',here,'blockchainToValidate[0]',blockchainToValidate[0]);
-	console.log('Erreur dans',here,'mais blockGenesis',JSON.stringify(getGenesisBlock()),'\n');
-        return false;
+	console.log('\n');
+	console.log('dans',here,'genesisBlockToValidate.horodatage',genesisBlockToValidate.horodatage);
+	console.log('dans',here,'   genesisBlockCurrent.horodatage',genesisBlockCurrent.horodatage);
+
+	if (genesisBlockToValidate.horodatage < genesisBlockCurrent.horodatage) {
+	    var result = true;
+	} else {
+	    console.log('\n');
+	    console.log('Erreur dans',here,'genesisBlockToValidate.horodatage',genesisBlockToValidate.horodatage);
+	    console.log('Erreur dans',here,'tandis que genesisBlockCurrent.horodatage',genesisBlockCurrent.horodatage);
+	    console.log('\n');
+	    console.log('Sortie  de',here);
+	    var result = false;
+	}
+	console.log('Sortie  de',here,'avec result',result);
+	return result;
     }
+    
     var tempBlocks = [blockchainToValidate[0]];
     for (var i = 1; i < blockchainToValidate.length; i++) {
         if (isValidNewBlock (blockchainToValidate[i], tempBlocks[i - 1], here)) {
@@ -57,7 +98,10 @@ var isValidChain = (blockchainToValidate) => {
 
 var isValidNewBlock = (newBlock, previousBlock, caller) => {
     var here = O.functionNameJS (ModuleName);
-    console.log('Entrée dans',here,'appelé par',caller,'avec newBlock',newBlock,'previousBlock',previousBlock);
+    console.log('\n');
+    console.log('Entrée dans',here,'appelé par',caller)
+    console.log('Entrée dans',here,'avec newBlock',newBlock)
+    console.log('Entrée dans',here,'avec previousBlock',previousBlock);
     
     if (previousBlock.index + 1 !== newBlock.index) {
         console.log('Dans',here,'index invalide previousBlock.index',previousBlock.index,'newBlock.index',newBlock.index);
@@ -72,7 +116,8 @@ var isValidNewBlock = (newBlock, previousBlock, caller) => {
         console.log(typeof (newBlock.hashCourant) + ' ' + typeof O.calculateHashForBlock(newBlock));
         console.log('\nErreur dans',here,'hashCourant',O.calculateHashForBlock(newBlock))
 	console.log('Erreur est différent de newBlock.hashCourant',newBlock.hashCourant);
-	console.log('\nSortie  de',here);
+	console.log('\n');
+	console.log('Sortie  de',here);
         return false;
     }
     console.log('Sortie  de',here,'result true');
@@ -81,19 +126,63 @@ var isValidNewBlock = (newBlock, previousBlock, caller) => {
 
 var replaceChain = (newBlocks, caller) => {
     var here = O.functionNameJS(ModuleName);
-    console.log('Entrée dans',here,'appelé par',caller,'avec newBlocks',newBlocks);
+    console.log('\n');
+    console.log('Entrée dans',here,'appelé par',caller);
+    console.log('Entrée dans',here,'avec newBlocks',newBlocks);
+    var areValidNewBlocks = isValidChain(newBlocks);
+
+    console.log('dans',here,'areValidNewBlocks',areValidNewBlocks);
+    console.log('dans',here,'newBlocks.length',newBlocks.length);
+    console.log('dans',here,'blockChain.length',A.blockChain.length);
     
-    if (isValidChain(newBlocks) && newBlocks.length > blockChain.length) {
-        console.log('La blockchain reçue est valide. Remplacer la blockchain actuelle par la blockchain reçue.');
+    if (isValidChain(newBlocks) && newBlocks.length > A.blockChain.length) {
+        console.log('La blockchain reçue est valide.');
+        console.log('Remplacer la blockchain actuelle par la blockchain reçue.');
+        A.blockChain = newBlocks;
+        broadcast(responseLatestMsg(), here);
+    } else if (isValidChain(newBlocks) && ( (newBlocks.length == 1) && (newBlocks.length == A.blockChain.length) )) {
+        console.log('La blockchain reçue est valide.');
+        console.log('Remplacer la blockchain actuelle par la blockchain reçue.');
         A.blockChain = newBlocks;
         broadcast(responseLatestMsg(), here);
     } else {
-        console.log('\n!!!!! dans',here,'La blockchain reçue est invalide. !!!!!');
+	console.log('\n');
+        console.log('!!!!! dans',here,'La blockchain reçue est invalide. !!!!!');
+	console.log('\n');
     }
     console.log('Sortie  de',here);
 };
 
+var responseLatestMsg = () => {
+    var here = O.functionNameJS (ModuleName);
+    console.log('\n');
+    console.log('Entrée dans',here,'avec',A.blockChain.length,'blocs');
+
+    var latestE = getLatestBlock();
+    var result = {
+	'type': MessageType.RESPONSE_BLOCKCHAIN,
+	'data': JSON.stringify([latestE])
+    };
+    console.log('Sortie  de',here,'result',result);
+    return result;
+};
+
+var write = (ws, message, caller) => {
+    var here = O.functionNameJS (ModuleName);
+    console.log('\n');
+    console.log('Entrée dans',here,'appelé par',caller);
+    console.log('Entrée dans',here,'avec ws.url',ws.url);
+    console.log('Entrée dans',here,'avec message',message);
+    
+    ws.send(JSON.stringify(message))
+    console.log('Sortie  de',here);
+};
+
 module.exports.addBlock = addBlock;
+module.exports.broadcast = broadcast;
 module.exports.getGenesisBlock = getGenesisBlock;
 module.exports.getLatestBlock = getLatestBlock;
 module.exports.replaceChain = replaceChain;
+module.exports.responseLatestMsg = responseLatestMsg;
+module.exports.MessageType = MessageType;
+module.exports.write = write;
--- websocket.js
+++ websocket.js
@@ -11,21 +11,6 @@ const {Block} = require ('./block.js');
 
 var ModuleName = 'websocket.js';
 
-var MessageType = {
-    QUERY_LATEST: 0,
-    QUERY_ALL: 1,
-    RESPONSE_BLOCKCHAIN: 2
-};
-
-var broadcast = (message, caller) => {
-    var here = O.functionNameJS(ModuleName);
-    console.log('Entrée dans',here,'appelé par',caller,'avec message',message)
-    console.log('dans',here,'il y a',A.socket_a.length,'sockets');
-
-    A.socket_a.forEach (soc => write(soc, message, here));
-    console.log('Sortie  de',here);
-}
-
 var connectToPeers = (newPeers, caller) => {
     var here = O.functionNameJS(ModuleName); 
     console.log('Entrée dans',here,'appelé par',caller,'avec',newPeers.length,'newPeers');
@@ -67,62 +52,91 @@ var generateNextBlock = (blockData, caller) => {
 
 var handleBlockchainResponse = (message, caller) => {
     var here = O.functionNameJS(ModuleName);
-    console.log('\nEntrée dans',here,'appelé par',caller,'message',message);
-
+    console.log('\n');
+    console.log('Entrée dans',here,'appelé par',caller);
+    console.log('Entrée dans',here,'avec message',message);
+    
     /* tri sur les indices */
     var receivedBlocks = JSON.parse(message.data).sort((b1, b2) => (b1.index - b2.index));
     var latestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
     var latestBlockHeld = A.blockChain[A.blockChain.length - 1];
 
+    console.log('\n');
     console.log('Dans',here,'Tous les Blocs reçus:',receivedBlocks);
+        console.log('\n');
     console.log('Dans',here,'Dernier  Bloc stocké:',latestBlockHeld);
-    
+    console.log('\n');
     console.log('Dans',here,'Index du dernier block:',latestBlockHeld.index);
     console.log('Dans',here,'Index du block    reçu:',latestBlockReceived.index);
+    console.log('\n');
+    
     
     if (latestBlockReceived.index > latestBlockHeld.index) {
-        console.log('Dans',here,'Dernier block de la blockchain : ' + latestBlockHeld.index + '. Block reçu par le pair : ' + latestBlockReceived.index);
+        console.log('dans',here,'index du dernier Bloc de la blockchain :',latestBlockHeld.index);
+	console.log('dans',here,'index du bloc reçu par le pair :',latestBlockReceived.index);
 	
         if (latestBlockHeld.hashCourant === latestBlockReceived.hashPrecedent) {
-            console.log('Dans',here,'Les Hashes correspondent.Nous pouvons appondre le block reçu à notre chaîne et le diffuser');
+            console.log('Dans',here,'Les Hashes correspondent. Nous pouvons appondre le bloc reçu à notre chaîne et le diffuser');
             A.blockChain.push (latestBlockReceived);
-            broadcast (responseLatestMsg(), here);
+            B.broadcast (B.responseLatestMsg(), here);
         } else if (receivedBlocks.length === 1) {
             console.log('Dans',here,'Le block reçu a une longueur de 1. Nous devons interroger notre chaîne depuis notre pair');
-            broadcast(queryAllMsg(), here);
+            B.broadcast(queryAllMsg(), here);
         } else {
-            console.log('Dans',here,'La blockchain reçue est plus longue (',receivedBlocks.length,') que la blockchain actuelle (',latestBlockHeld.index,')');
-            replaceChain(receivedBlocks, here);
+            console.log('Dans',here,'La blockchain reçue est plus longue (',receivedBlocks.length,') que la blockchain actuelle (',latestBlockHeld.index,') la remplacer');
+            B.replaceChain(receivedBlocks, here);
         }
     } else {
 	if (latestBlockReceived.index == 0) {
-            console.log('Dans',here,'La blockchain a reçu un block genesis (index = 0). Ne rien faire.');
+            console.log('Dans',here,'La blockchain a reçu un Bloc Genesis (index = 0)');
+	    if (latestBlockReceived.horodatage < latestBlockHeld.horodatage) {
+		console.log('Dans',here,'l\'horodatage du Bloc reçu (',latestBlockReceived.horodatage,') < à celui Bloc courant (',latestBlockHeld.horodatage,') le remplacer');
+		B.replaceChain(receivedBlocks, here);
+	    }
 	} else {
-	       console.log('Dans',here,'La blockchain reçue est plus courte (',receivedBlocks.length,') que la blockchain actuelle (',latestBlockHeld.index+1,'). Ne rien faire.');
+	    console.log('Dans',here,'La blockchain reçue est plus courte (',receivedBlocks.length,') que la blockchain actuelle (',latestBlockHeld.index+1,'). Ne rien faire.');
 	}
     }
+    console.log('\n');
+    console.log('Blockchain:');
+    console.log(A.blockChain);
+    console.log('\n');
     console.log('Sortie  de',here);
-    console.log('\n===== Bloc #',latestBlockReceived.index,'fin de handleBlockchainResponse =====');
+    console.log('\n');
+    console.log('===== Bloc #',latestBlockReceived.index,'fin de handleBlockchainResponse =====');
 };
 
 var initConnection = (ws, caller) => {
     var here = O.functionNameJS (ModuleName);
-    console.log('\n===== Entrée dans',here,'nouvelle connexion à',ws.url,'=====\n');
+    console.log('\n');
+    console.log('===== Entrée dans',here,'appelé par',caller,'=====');
+    console.log('\n');
+    console.log('===== Entrée dans',here,'nouvelle connexion à',ws.url,'=====');
+
+    if (ws == undefined ) {
+	console.log('\n');
+	console.log('WARNING dans',here,'ws est undefined');
+    }
     
     A.socket_a.push(ws);
+    console.log('\n');
     console.log('dans',here,'ws pour ws.url',ws.url,'ajouté à A.socket_a');
-    console.log('dans',here,'socket_a',A.socket_a);
-
+    console.log('dans',here,'socket_a a',A.socket_a.length,'sockets');
+    
     initMessageHandler(ws, here);
+    console.log('\n');
     initErrorHandler(ws, here);
-
-    console.log('Dans',here,'écriture de queryChainLengthMsg',queryChainLengthMsg(),'dans ws.url',ws.url);
-    write(ws, queryChainLengthMsg(), here);
+    console.log('\n');
+    
+    console.log('dans',here,'écriture de queryChainLengthMsg',queryChainLengthMsg(),'dans ws.url',ws.url);
+    
+    B.write(ws, queryChainLengthMsg(), here);
     console.log('      Sortie  de',here);
 };
 
 var initErrorHandler = (ws, caller) => {
     var here = O.functionNameJS (ModuleName);
+    console.log('\n');
     console.log('Entrée dans',here,'appelé par',caller);
 
     var closeConnection = (ws) => {
@@ -136,7 +150,8 @@ var initErrorHandler = (ws, caller) => {
 
 var initHttpServer = (http_port, app, caller) => {
     var here = O.functionNameJS(ModuleName);
-    console.log('\n===== Entrée dans',here,'appelé par',caller,'=====');
+    console.log('\n');
+    console.log('===== Entrée dans',here,'appelé par',caller,'=====');
 
     app.use(bodyParser.json());
 
@@ -147,7 +162,7 @@ var initHttpServer = (http_port, app, caller) => {
 	console.log('     dans',here,'/mineBlock avec req.body',req.body);
         var newBlock = generateNextBlock(req.body.contenu, here);
         B.addBlock(newBlock, here);
-        broadcast(responseLatestMsg(), here);
+        B.broadcast(B.responseLatestMsg(), here);
         console.log('Dans',here,'ajout et diffusion du bloc',JSON.stringify(newBlock));
         res.send();
     });
@@ -161,8 +176,10 @@ var initHttpServer = (http_port, app, caller) => {
         res.send();
     });
     
-    app.listen(http_port, () =>
-	       console.log('\n===== Dans',here,'Écoute sur le port HTTP', http_port,'====='));
+    app.listen(http_port, () => {
+	console.log('\n');
+	console.log('===== Dans',here,'Écoute sur le port HTTP', http_port,'=====');}
+	      );
 
     console.log('      Sortie  de',here);
 };
@@ -170,10 +187,15 @@ var initHttpServer = (http_port, app, caller) => {
 var initMessageHandler = (ws, caller) => {
     var here = O.functionNameJS (ModuleName);
 
-    console.log('\n===== Entrée dans',here,'appelé par',caller,'avec ws.url',ws.url,'=====\n');
+    console.log('\n');
+    console.log('===== Entrée dans',here,'appelé par',caller,'=====');
+    console.log('===== Entrée dans',here,'avec ws.url',ws.url,'=====');
+    console.log('\n');
     console.log('dans',here,'il y a',A.socket_a.length,'sockets');
+    console.log('\n');
     console.log('dans',here,'blockChain',A.blockChain);
-
+    console.log('\n');
+    
     if (A.socket_a.length < 1) {
 	O.errorMessage ('socket_a.length > 0',A.socket_a.length, '?', here)
     }
@@ -183,16 +205,18 @@ var initMessageHandler = (ws, caller) => {
 
         console.log('Dans',here,'ws.on Message Reçu data',data);
 	console.log('Dans',here,'message.type',message.type);
+	console.log('\n');
+	
         switch (message.type) {
-        case MessageType.QUERY_LATEST:
-	    console.log('Dans',here,'write dans',ws.url,'de responseLatestMsg', responseLatestMsg());
-            write(ws, responseLatestMsg(), here);
+        case B.MessageType.QUERY_LATEST:
+	    console.log('Dans',here,'write dans',ws.url,'de responseLatestMsg', B.responseLatestMsg());
+            B.write(ws, B.responseLatestMsg(), here);
             break;
-        case MessageType.QUERY_ALL:
+        case B.MessageType.QUERY_ALL:
 	    console.log('Dans',here,'appel de responseChainMsg');
-            write(ws, responseChainMsg(), here);
+            B.write(ws, B.responseChainMsg(), here);
             break;
-        case MessageType.RESPONSE_BLOCKCHAIN:
+        case B.MessageType.RESPONSE_BLOCKCHAIN:
 	    console.log('Dans',here,'appel de handleBlockchainResponse');
             handleBlockchainResponse (message, here)
             break;
@@ -203,55 +227,35 @@ var initMessageHandler = (ws, caller) => {
 
 var initP2PServer = (p2p_port, caller) => {
     var here = O.functionNameJS (ModuleName);
-    console.log('\n===== Entrée dans',here,'appelé par',caller,'avec p2p_port',p2p_port,'=====',);
+    console.log('\n');
+    console.log('===== Entrée dans',here,'appelé par',caller,'=====',);
+    console.log('===== Entrée dans',here,'avec p2p_port',p2p_port,'=====',);
+    console.log('\n');
     
     var server = new WebSocket.Server({port: p2p_port});
     server.on('connection', ws =>
-	      initConnection(ws, here)
+		  initConnection(ws, here)
 	     );
     console.log('============ dans',here,'création du WebSocket.Server en Écoute du port P2P sur',p2p_port,'=====');
-
     console.log('      Sortie  de ',here);
 };
 
-var queryChainLengthMsg = () => ({'type': MessageType.QUERY_LATEST});
+var queryChainLengthMsg = () => ({'type': B.MessageType.QUERY_LATEST});
 
-var queryAllMsg = () => ({'type': MessageType.QUERY_ALL});
+var queryAllMsg = () => ({'type': B.MessageType.QUERY_ALL});
 
 var responseChainMsg = () => {
     var here = O.functionNameJS (ModuleName);
-    console.log('Entrée dans',here,'avec A.blockChain',A.blockChain);
-    console.log('dans',here,'il y a',A.blockChain.length,'blocs');
+    console.log('\n');
+    console.log('Entrée dans',here,'avec',A.blockChain.length,'blocs');
     
     var result = {
-	'type': MessageType.RESPONSE_BLOCKCHAIN,
+	'type': B.MessageType.RESPONSE_BLOCKCHAIN,
 	'data': JSON.stringify(A.blockChain)
     }
 
     console.log('Sortie  de',here,'result',result);
     return result;
-};
-
-var responseLatestMsg = () => {
-    var here = O.functionNameJS (ModuleName);
-    console.log('Entrée dans',here)
-    console.log('dans',here,'il y a',A.blockChain.length,'blocs');
-
-    var latestE = B.getLatestBlock();
-    var result = {
-	'type': MessageType.RESPONSE_BLOCKCHAIN,
-	'data': JSON.stringify([latestE])
-    };
-    console.log('Sortie  de',here,'result',result);
-    return result;
-};
-
-var write = (ws, message, caller) => {
-    var here = O.functionNameJS (ModuleName);
-    console.log('Entrée dans',here,'appelé par',caller,'avec ws.url',ws.url,'message',message);
-    
-    ws.send(JSON.stringify(message))
-    console.log('Sortie  de',here);
 };
 
 var printWebSocketOn = (ws, caller) => {
